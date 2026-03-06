a18b980989fadab3ae19139e5eeef2af41841138
Aaryaman Singh <aaryamansingh49@gmail.com>
2026-03-06 21:52:02 +0530

    mm

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,11 @@ require("./config/redis");
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: "*",
-  methods: ["GET","POST","PUT","DELETE"],
-  allowedHeaders: ["Content-Type","Authorization"]
-}));
+// app.use(cors({
+//   origin: "*",
+//   methods: ["GET","POST","PUT","DELETE"],
+//   allowedHeaders: ["Content-Type","Authorization"]
+// }));
 app.options("*", cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
--- front/src/api/courseApi.js
+++ front/src/api/courseApi.js
@@ -1,7 +1,14 @@
 import API from "./axiosInstance";
 
-export const getCourses = (program, semester) =>
-  API.get(`/courses?program=${program}&semester=${semester}`);
+export const getCourses = (program, semester) => {
+
+  const params = new URLSearchParams();
+
+  if (program) params.append("program", program);
+  if (semester) params.append("semester", semester);
+
+  return API.get(`/courses?${params.toString()}`);
+};
 
 export const getRecentCourses = () =>
   API.get("/worksheets/recent-courses");
