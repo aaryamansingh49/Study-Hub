0afd0a05f0ef490c9bb8ee6320528460e4bccb36
Aaryaman Singh <aaryamansingh49@gmail.com>
2026-03-06 22:06:50 +0530

    fix cors origin

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "*",
+  origin:   "http://localhost:5173",
   methods: ["GET","POST","PUT","DELETE"],
   allowedHeaders: ["Content-Type","Authorization"]
 }));
