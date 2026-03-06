6c49fa96431bc3d57eaddee9efd2e9c93842b78b
Aaryaman Singh <aaryamansingh49@gmail.com>
2026-03-06 22:02:51 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ require("./config/redis");
 const app = express();
 
 // Middleware
-// app.use(cors({
-//   origin: "*",
-//   methods: ["GET","POST","PUT","DELETE"],
-//   allowedHeaders: ["Content-Type","Authorization"]
-// }));
-app.options("*", cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","DELETE"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
+// app.options("*", cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
